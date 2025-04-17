eb3b7417ab1dd27f9db6286d7079647156742f44
xWires <xwires@tangledwires.xyz>
2025-04-17 22:58:32 +0100

    Put each character in its own <span> element

--- pages/connect.vue
+++ pages/connect.vue
@@ -6,7 +6,7 @@ const bridge_msg_prefix = "[bridge] ";
 // The address of the bridge (https://github.com/prestel-uk/web-bridge) to connect to
 const bridge_addr = "ws://127.0.0.1:1234";
 
-const output = useTemplateRef("output");
+const outputContainer = useTemplateRef("outputContainer");
 const toggleConnectedButton = useTemplateRef("toggleConnectedButton");
 
 let connected = false;
@@ -20,13 +20,19 @@ function parseResponse(response) {
   }
 
   let json = JSON.parse(response);
-  let result = "";
   for (const i in json) {
     // The response is received as a JSON array of character codes as numbers which need to have the parity bit removed, then converted into a string
     const withoutParity = json[i] & 0b01111111;
-    result += String.fromCharCode(withoutParity);
+    // Create a new <span> element with a unique id so that it can be referenced later, and set the content to the character. If a span already exists for that character, use that instead.
+    let charSpan = document.getElementById("char" + i);
+    if (!charSpan) {
+      charSpan = document.createElement("span");
+    }
+
+    charSpan.id = "char" + i;
+    charSpan.innerText = String.fromCharCode(withoutParity);
+    outputContainer.value.appendChild(charSpan);
   }
-  return result;
 }
 
 function connect() {
@@ -36,14 +42,14 @@ function connect() {
   }
   ws.onmessage = (event) => {
     console.log(event.data);
-    output.value.innerText = parseResponse(event.data);
+    parseResponse(event.data);
   }
   connected = true;
 }
 
 function disconnect() {
   ws.close();
-  output.value.innerText = "";
+  outputContainer.value.innerHTML = "";
   toggleConnectedButton.value.innerText = "Connect";
   connected = false;
 }
@@ -67,7 +73,7 @@ onMounted(() => {
 
 <template>
   <div>
-    <p ref="output"></p>
+    <div ref="outputContainer"></div>
     <button ref="toggleConnectedButton" @click="toggleConnected">Connect</button>
   </div>
 </template>
