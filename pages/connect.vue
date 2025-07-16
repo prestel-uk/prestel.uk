e7d4f619765855482d7da280c725bf111f06ba40
xWires <xwires@tangledwires.xyz>
2025-07-16 12:11:53 +0100

    Use HTMLDisplayAdaptor in vtxcore 0.5.0

--- pages/connect.vue
+++ pages/connect.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import "~/assets/css/terminal.css";
+import "~/assets/css/vtxcore.css";
 import {useTemplateRef} from "vue";
-import init, {Colour, MosaicType, VTXScreen} from "@prestel-uk/vtxcore";
+import init, {VTXScreen} from "@prestel-uk/vtxcore";
 
 // The offset of contiguous mosaics in the font
 const CONTIGUOUS_MOSAICS_OFFSET = 57856;
@@ -16,7 +16,7 @@ const BRIDGE_ADDR = "ws://127.0.0.1:1234";
 const ROWS = 24;
 const COLUMNS = 40;
 
-const outputGrid = useTemplateRef("outputGrid");
+const outputContainer = useTemplateRef("outputContainer");
 const toggleConnectedButton = useTemplateRef("toggleConnectedButton");
 
 let connected = false;
@@ -27,22 +27,6 @@ let ws: WebSocket;
 function clearTerminal() {
   screen.clear();
   drawScreen();
-}
-
-// Get a mosaic character using the number sent by the server
-function getMosaicChar(character: string, mosaicType: MosaicType) {
-  const code = character.charCodeAt(0);
-
-  // The 6th bit is used for something else which I don't understand at the moment, so for now it just gets ignored
-  const maskedCode = code & 0b01011111;
-  switch (mosaicType) {
-    case MosaicType.Contiguous:
-      return String.fromCodePoint(maskedCode + CONTIGUOUS_MOSAICS_OFFSET);
-    case MosaicType.Separated:
-      return String.fromCodePoint(maskedCode + SEPARATED_MOSAICS_OFFSET);
-    default:
-      return "";
-  }
 }
 
 async function parseResponse(response: Blob) {
@@ -59,41 +43,7 @@ async function parseResponse(response: Blob) {
 }
 
 function drawScreen() {
-  // Keep track of where in the grid we are
-  let idx = 0;
-  for (let c of screen.buffer) {
-    // If the character shouldn't be displayed, do nothing
-    if (c.to_be_displayed === undefined) {
-      continue;
-    }
-
-    // Each character has its own <span> element which is created when the page loads
-    let charSpan = document.getElementById("char" + idx);
-
-    let colour = "";
-
-    switch (c.colour) {
-      case Colour.Red: colour = "red"; break;
-      case Colour.Green: colour = "green"; break;
-      case Colour.Yellow: colour = "yellow"; break;
-      case Colour.Blue: colour = "blue"; break;
-      case Colour.Magenta: colour = "magenta"; break;
-      case Colour.Cyan: colour = "cyan"; break;
-      case Colour.White: colour = "white"; break;
-    }
-
-    let heightMode = "normal-height";
-
-    if (c.double_height) {
-      heightMode = "double-height";
-    }
-
-    charSpan!.innerText = c.is_mosaic ? getMosaicChar(c.to_be_displayed, c.mosaic_type) : c.to_be_displayed;
-    charSpan!.dataset.colour = colour;
-    charSpan!.dataset.heightmode = heightMode;
-
-    idx++;
-  }
+  outputContainer.value!.innerHTML = screen.display_for_html();
 }
 
 function connect() {
@@ -134,21 +84,12 @@ onMounted(() => {
       ws.send(event.key);
     }
   });
-
-  // Create the <span> elements for each character
-  for (let i = 0; i < ROWS * COLUMNS; i++) {
-    const element = document.createElement("span");
-
-    element.id = "char" + i;
-
-    outputGrid.value!.appendChild(element);
-  }
 });
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center">
-    <div ref="outputGrid" id="outputGrid"></div>
+    <div ref="outputContainer"></div>
     <br>
     <button ref="toggleConnectedButton" @click="toggleConnected">Connect</button>
   </div>
